115461636576abc3b0eebfe20a18eb7cf71ff1ed
aminasul <58600015+aminasul@users.noreply.github.com>
2020-03-09 19:09:20 +0100

    sjuk e du

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,14 @@
 
 public class Game {
+	
+	private static void doStuff() {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		doStuff();
 	}
 
 }
